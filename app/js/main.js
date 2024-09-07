ec60cded7189513fa568daec1fda2dc4bf9ff24e
Ruslan <camarederic@gmail.com>
2024-09-07 20:57:46 +0300

    added a hamburger and a pop-up menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$(function () {
-  
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".menu__close");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.add("active");
 });
+
+close.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+$(function () {});
